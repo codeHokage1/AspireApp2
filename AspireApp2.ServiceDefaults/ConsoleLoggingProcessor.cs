9e64d63a67957dc24ad51cf0e13d076ad49db12d
Farhan Sodiq <sodiqfarhan360@gmail.com>
2024-09-16 23:09:43 +0100

    Add project files.

---
+++ AspireApp2.ServiceDefaults/ConsoleLoggingProcessor.cs
@@ -0,0 +1,22 @@
+﻿using OpenTelemetry.Logs;
+using OpenTelemetry;
+using Microsoft.Extensions.Logging;
+
+namespace AspireApp2.ServiceDefaults
+{
+    public class ConsoleLoggingProcessor : BaseProcessor<LogRecord>
+    {
+        private readonly ILogger<ConsoleLoggingProcessor> _logger;
+
+        public ConsoleLoggingProcessor(ILogger<ConsoleLoggingProcessor> logger)
+        {
+            _logger = logger;
+        }
+
+        public override void OnEnd(LogRecord data)
+        {
+            base.OnEnd(data);
+            _logger.LogInformation("Log sent to endpoint: {FormattedMessage}", data.FormattedMessage);
+        }
+    }
+}
